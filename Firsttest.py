6108622d11d599db988d1b09a73f244617aaddae
arjtar <arjantpaudel@gmail.com>
2025-05-09 23:49:36 +0545

    Second

--- Firsttest.py
+++ Firsttest.py
@@ -1,13 +1,8 @@
-# from selenium import webdriver
-# driver = webdriver.Chrome("C:\testdrive\chromedriver-win64\chromedriver-win64\chromedriver.exe")
-# driver.get("https://the-internet.herokuapp.com/login")
-
-# driver.find_element_by_name("txtusername").send_keys("tomsmith")
-# driver.find_element_by_id("txtPassword").send_keys("SuperSecretPassword!")
-
-
 from selenium import webdriver
-
-driver = webdriver.Chrome(r"C:\testdrive\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome("C:\testdrive\chromedriver-win64\chromedriver-win64\chromedriver.exe")
 driver.get("https://the-internet.herokuapp.com/login")
+
+driver.find_element_by_name("txtusername").send_keys("tomsmith")
+driver.find_element_by_id("txtPassword").send_keys("SuperSecretPassword!")
+
 
